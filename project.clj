50c7cd62d5b24eec749cbdcb66a1a8a1948e7090
Jordan Robinson <me@jordanrobinson.co.uk>
2023-12-11 11:37:02 +0000

    Testing out github actions

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   (or (System/getenv "VERSION")
       "2.0.0"))
 
-(defproject ai.mypulse/placid-fish version
+(defproject uk.co.jordanrobinson/placid-fish version
   :description "Safe url assertions for test classes that uses Exploding Fish under the hood"
-  :url "https://github.com/Global-Online-Health/placid-fish"
+  :url "https://github.com/jordanrobinson/placid-fish"
 
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
